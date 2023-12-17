f67404d9d7a75cab4ae7481eb3985103b5ecdfdd
raccoon-dog-p <qkrdydtjr02@naver.com>
2023-12-17 18:22:08 +0900

    feat: front navibar 컴포넌트 및 스타일 작성

---
+++ coffee_delivery/frontend/src/components/NaviBar.js
@@ -0,0 +1,42 @@
+import styles from '../styles/NaviBar.module.css'
+import { Link } from 'react-router-dom';
+
+export default function NaviBar() {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+            <Link to='/'>
+                <img src='img/logo.svg' style={styles.img}></img>
+            </Link>
+                <ul className={styles.ul}>
+                    <Link to='/reservation' style={styles.a}>
+                    <li className={styles.li}>예약 하기</li>
+                    </Link>
+                    <Link to='/beans' style={styles.a}>
+                    <li className={styles.li}>원두 소개</li>
+                    </Link>
+                    <Link to='/menu' style={styles.a}>
+                    <li className={styles.li}>메뉴 소개</li>
+                    </Link>
+                    <Link to='/notice' style={styles.a}>
+                    <li className={styles.li}>공지 사항</li>
+                    </Link>
+                </ul>
+                <ul className={styles.right_ul}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}>
+                            <Link to='/signin'>
+                            <button>로그인/회원가입</button>
+                            </Link>
+                        </li>
+                        <div className={styles.div}>
+                            <Link to='/cart'>
+                            <img src='img/cart.svg'></img>
+                            </Link>
+                        </div>
+                    </ul>
+                </ul>
+            </nav>
+        </header>
+    );
+}
